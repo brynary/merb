a7aace1fa2e550fc88dfcc4fd7112d2c422da2c1
Bryan Helmkamp <bryan@brynary.com>
2009-06-17 01:09:14 -0400

    Refactoring request helper

--- merb-core/lib/merb-core/test/helpers/request_helper.rb
+++ merb-core/lib/merb-core/test/helpers/request_helper.rb
@@ -4,30 +4,45 @@ require "rack/test"
 module Merb
   module Test
     module MakeRequest
+      include ::Rack::Test::Methods
+
+      def app
+        Merb::Config[:app]
+      end
+
+      def build_rack_mock_session
+        mock_session = super
+
+        mock_session.after_request do
+          process_work_queue
+        end
+
+        return mock_session
+      end
 
       def request(uri, env = {})
+        with_session_for_jar(env) do |session|
+          session.request(uri.to_s, env)
+        end
+      end
+      
+      def with_session_for_jar(env, &block)
         if env.has_key?(:jar) && env[:jar].nil?
-          jar = nil
+          session_name = false
         else
-          jar = env.delete(:jar) || :default
+          session_name = env.delete(:jar) || :default
         end
         
-        response = get_session(jar).request(uri.to_s, env)
+        with_session(session_name, &block)
+      end
 
+      def process_work_queue
         Merb::Dispatcher.work_queue.size.times do
           Merb::Dispatcher.work_queue.pop.call
         end
-
-        response
-      end
-      
-      def get_session(name = :default)
-        return ::Rack::Test::Session.new(Merb::Config[:app]) unless name
-        @__sessions__ ||= {}
-        @__sessions__[name] ||= ::Rack::Test::Session.new(Merb::Config[:app])
       end
     end
-    
+
     module RequestHelper
       include MakeRequest
 
@@ -44,11 +59,11 @@ module Merb
           input
         end
       end
-      
+
       def status_code(input)
         input.respond_to?(:status) ? input.status : input
       end
-      
+
       def requesting(*args)   request(*args) end
       def response_for(*args) request(*args) end
     end
